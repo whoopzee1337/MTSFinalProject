a27e3aeda06fb1dc78b1beb4b8e703e652246cba
whoopzee1337 <whoopzee228@icloud.com>
2023-04-18 19:59:13 +0300

    initial commit

---
+++ src/main/java/com/example/mtsfinalproject/MtsFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtsfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtsFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MtsFinalProjectApplication.class, args);
+    }
+
+}
